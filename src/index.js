3d2079b5fc8879eb0038e3adb7512b08f41a0701
xuyifeng <52asdasd@qwq.com>
2021-07-20 18:08:31 +0800

    懒加载

--- src/index.js
+++ src/index.js
@@ -1,10 +1,19 @@
-async function getComponent() {
+import _ from 'lodash';
+
+function component() {
   const element = document.createElement("div");
-  const { default: _ } = await import("lodash");
+  const button = document.createElement("button");
+  const br = document.createElement("br");
+
+  button.innerHTML = 'Click me and look at the console!';
   element.innerHTML = _.join(["Hello", "webpack"], " ");
+  element.appendChild(br);
+  element.appendChild(button);
+  button.onclick = e => import(/* webpackChunkName: "print" */"./print").then(module => {
+    const print = module.default;
+    print();
+  })
   return element;
 }
 
-getComponent().then((component) => {
-  document.body.appendChild(component);
-});
+document.body.appendChild(component());
