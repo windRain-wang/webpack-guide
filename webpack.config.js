60208d39ed725c31de3e83d3aa46de19eef3e080
xuyifeng <52asdasd@qwq.com>
2021-07-21 18:23:25 +0800

    环境变量

--- webpack.config.js
+++ webpack.config.js
@@ -1,9 +1,15 @@
-const path = require('path');
+// 使用 npx webpack --env goal=local --env production --progress 打包，可以看出效果
+const path = require("path");
 
-module.exports = {
-    entry: './src/index.js',
+module.exports = (env) => {
+  console.log("Goal: ", env.goal);
+  console.log("Production: ", env.production);
+
+  return {
+    entry: "./src/index.js",
     output: {
-        filename: 'main.js',
-        path: path.join(__dirname, 'dist'),
-    }
-}
+      filename: "main.js",
+      path: path.join(__dirname, "dist"),
+    },
+  };
+};
