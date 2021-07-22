0ce71410ea65b99626ec97d1f2db53e5f0950613
xuyifeng <52asdasd@qwq.com>
2021-07-22 15:38:06 +0800

    渐进式网络应用程序

--- webpack.config.js
+++ webpack.config.js
@@ -1,25 +1,29 @@
 const path = require("path");
 const HtmlWebpackPlugin = require("html-webpack-plugin");
+const WorkboxPlugin = require('workbox-webpack-plugin');
 
 module.exports = {
-  mode: "development",
   entry: {
     index: "./src/index.js",
     print: "./src/print.js",
   },
-  devtool: "inline-source-map",
   devServer: {
-    contentBase: "./dist"
+    writeToDisk: true,
   },
   plugins: [
     new HtmlWebpackPlugin({
-      title: 'Development'
-    })
+      title: 'Progressive Web Application'
+    }),
+    new WorkboxPlugin.GenerateSW({
+      // 这些选项帮助快速启用 ServiceWorkers
+      // 不允许遗留任何“旧的” ServiceWorkers
+      clientsClaim: true,
+      skipWaiting: true,
+    }),
   ],
   output: {
     filename: '[name].bundle.js',
     path: path.resolve(__dirname, 'dist'),
-    clean: true,
-    publicPath: '/'
+    clean: true
   },
 };
