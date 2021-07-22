35ec2b9c8f9bd2aed0ed86de63d1957efd7f3aff
xuyifeng <52asdasd@qwq.com>
2021-07-22 16:46:18 +0800

    entry 的高级用法

--- webpack.config.js
+++ webpack.config.js
@@ -1,9 +1,30 @@
-const path = require('path');
-
+const path = require("path");
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 module.exports = {
-    entry: './src/index.js',
-    output: {
-        filename: 'main.js',
-        path: path.join(__dirname, 'dist'),
-    }
-}
+  mode: 'development',
+  entry: {
+    home: ["./src/home.js", "./src/home.scss"],
+    account: ["./src/account.js", "./src/account.scss"],
+  },
+  output: {
+    filename: "[name].js",
+    path: path.join(__dirname, "dist"),
+  },
+  module: {
+    rules: [
+      {
+        test: /\.scss$/,
+        use: [
+          MiniCssExtractPlugin.loader,
+          "css-loader",
+          "sass-loader",
+        ],
+      }
+    ],
+  },
+  plugins: [
+    new MiniCssExtractPlugin({
+      filename: "[name].css",
+    }),
+  ],
+};
