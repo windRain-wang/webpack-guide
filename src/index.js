cdf0cab669db0df9504d7404e84eb764a7cd52de
xuyifeng <52asdasd@qwq.com>
2021-07-22 14:28:11 +0800

    shamming

--- src/index.js
+++ src/index.js
@@ -1,11 +1,20 @@
-import _ from 'lodash'
-
 function component() {
-    const element = document.createElement('div');
+  const element = document.createElement("div");
 
-    element.innerHTML = _.join(['Hello', 'webpack'], ' ');
-
-    return element;
+  element.innerHTML = join(["Hello", "webpack"], " ");
+  this.alert("Hmmm, this probably isn't a great idea...");
+  return element;
 }
-
 document.body.appendChild(component());
+
+fetch("https://jsonplaceholder.typicode.com/users")
+  .then((response) => response.json())
+  .then((json) => {
+    console.log(
+      "We retrieved some data! AND we're confident it will work on a variety of browser distributions."
+    );
+    console.log(json);
+  })
+  .catch((error) =>
+    console.error("Something went wrong when fetching this data: ", error)
+  );
--- webpack.config.js
+++ webpack.config.js
@@ -1,9 +1,30 @@
-const path = require('path');
+const path = require("path");
+const webpack = require("webpack");
 
 module.exports = {
-    entry: './src/index.js',
-    output: {
-        filename: 'main.js',
-        path: path.join(__dirname, 'dist'),
-    }
-}
+  entry: {
+    polyfills: "./src/polyfills",
+    index: "./src/index.js",
+  },
+  output: {
+    filename: "[name].bundle.js",
+    path: path.join(__dirname, "dist"),
+  },
+  module: {
+    rules: [
+      {
+        test: require.resolve("./src/index.js"),
+        use: "imports-loader?wrapper=window",
+      },
+      {
+        test: require.resolve("./src/globals.js"),
+        use: "exports-loader?type=commonjs&exports=file,multiple|helpers.parse|parse",
+      },
+    ],
+  },
+  plugins: [
+    new webpack.ProvidePlugin({
+      join: ["lodash", "join"],
+    }),
+  ]
+};
